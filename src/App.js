001aa38e06ea845eb8ac4b94c0afb35db2077368
mepradippaudel <pradippaudel2052@gmail.com>
2021-09-15 21:45:02 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./actions";
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>increment/decrement by Redux</h1>
+      <button onClick={() => dispatch(incNumber(5))}>increment</button>
+      <span>{myState}</span>
+      <button onClick={() => dispatch(decNumber())}>decrement</button>
     </div>
   );
-}
-
+};
 export default App;
